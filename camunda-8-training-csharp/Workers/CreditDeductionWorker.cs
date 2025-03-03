d73de4aa6922d228c78da26abca942a9836c475b
TalkinWhip <52785781+TalkinWhip@users.noreply.github.com>
2025-03-03 15:16:59 +0100

    Update CreditDeductionWorker.cs

--- camunda-8-training-csharp/Workers/CreditDeductionWorker.cs
+++ camunda-8-training-csharp/Workers/CreditDeductionWorker.cs
@@ -14,18 +14,6 @@ public class CreditDeductionWorker : Worker {
     public override void Handler(IJobClient jobClient, IJob activatedjob)
     {
         Console.Out.WriteLine("Worker invoked: " + activatedjob.Type);
-
-        CreditService customerService = new CreditService();
-        Dictionary<string, object> variables = JsonConvert.DeserializeObject<Dictionary<string, object>>(activatedjob.Variables);
-
-        double orderTotal = Convert.ToDouble(variables["orderTotal"]); 
-        string custId = variables["customerId"].ToString();
-        double customerCredit = customerService.GetCustomerCredit(custId);
-
-        double openAmount = customerService.DeductCredit(customerCredit, orderTotal);
-        variables.Add("openAmount", openAmount);
-
-        string json = JsonConvert.SerializeObject(variables);
         client.NewCompleteJobCommand(activatedjob.Key).Variables(json).Send();   
     }
 }
